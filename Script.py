40801a5c9e20dfa6029b545dafcf4ab980057108
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-28 23:07:01 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,8 +1,91 @@
-class script(object):
-    START = """Hello, I am an automatic channel forwarding bot."""
-    
-    ABOUT = """
-<b>📝 Language:</b> Python 3
-<b>🧰 Framework:</b> Pyrogram
-<b>😎 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
-<b>🌐 Source Code:</b> Privet 🥰"""
+class script:
+    START = """Hi {user}! I am the one and only DRM Downloader Bot on [Telegram](https://telegram.org/).
+
+I can help you download content from OTT Platforms.
+
+You can use me to download DRM protected links to [Telegram](https://telegram.org/) & [Google Drive](https://drive.google.com/).
+
+Here, I support a vast number of OTTs from where you can easily download DRM or non-DRM videos.
+
+If you encounter any issues, please contact [Support](https://t.me/xyz)."""
+
+    ABOUT = """🤖 My Name - {botname}
+
+🏷️ Bot Version - <code>v8.0.17</code>
+📝 Language - [Python](https://python.org/)
+📚 Library - [Pyrogram](http://Pyrogram.org/)
+📡 Hosted on - [Heroku](http://heroku.com/)
+👨‍💻 Developer - Unknown
+📢 Updates Channel - [Support](https://t.me/xyz)
+🕵️ Buy Subscription - [Admin](https://t.me/xyz)"""
+
+    HELP = """Hi! Here you can find all available commands:
+
+/start - To start the bot
+/help - To know how to use the bot
+/features - To see available features
+/plans - To see available plans
+/usage - To see your current usage
+/otts - To know available OTTs
+
+1. Send URL (example.domain/File.mp4 | New Filename.mp4).
+2. Send Image As Custom Thumbnail (Optional).
+3. Select the button (Quality).
+
+Just send me any DRM links from supported OTTs to download, that I can also upload to [Google Drive](https://drive.google.com/) or [Telegram](https://telegram.org/).
+
+If the bot doesn't respond, contact [Admin](https://t.me/xyz)."""
+
+    CONTACT = """📞 Contact [Admin](https://t.me/xyz)"""
+
+    PLANS = """Here you will find all of our Premium Plans:-
+
+🔰BOT Plans🔰
+
+👇INDIVIDUAL PLANS (All OTTs)👇
+
+➡️ 1 Day     - 999 Coins 
+➡️ 7 Days    - 1499 Coins
+➡️ 28 Days   - 3499 Coins
+➡️ 84 Days   - 9999 Coins
+➡️ 365 Days  - 39999 Coins
+
+
+⚠️Term and Conditions⚠️
+
+• Payments are non-refundable, and we do not provide refunds. 
+• If the service ceases to function, no compensation is provided.
+
+👉 Contact to buy Subscription: [Admin](https://t.me/xyz)"""
+
+    OTT = """BOT's Available OTTs:
+
+1. Zee5
+2. SonyLiv
+3. Jio Cinema
+4. Kuku FM
+5. Dangal Play
+6. Sun NXT
+7. Hotstar
+8. Hoichoi
+9. MX Player
+10. ETV Win
+
+• Contact [Admin](https://t.me/xyz) to request more..."""
+
+    FEATURE = """Hi {users}! I am the first and only DRM downloader Bot on [Telegram](https://telegram.org/).
+
+• Send any DRM link and I will upload it to [Telegram](https://telegram.org/) or [Google Drive](https://drive.google.com/).
+
+• I support direct DRM links from Zee5, Sunnxt, Hoichoi, Aha, Sonyliv, Amazon Prime Video, Hotstar and Jio Cinema  (beta)
+
+• I support direct downloading from Amazon Prime Video (coming soon).
+
+• Support full season and series fetching in Hoichoi, Aha, AltBalaji, Shemaroome, and Amazon Prime Video (beta).
+
+• Support high bitrate video and DD+ audio from Jio Cinema.
+
+• More OTTs will be supported soon.
+
+• Auto thumbnail support."""
+
