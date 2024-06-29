369a53f57bee9d5d28f1132167895d77504c4ead
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-29 22:57:45 +0530

    Create users_chats_db.py

---
+++ database/users_chats_db.py
@@ -0,0 +1,83 @@
+import motor.motor_asyncio
+from info import DATABASE_NAME, DATABASE_URI, REFER_ON, DAILY_BONUS, MYSTORE
+
+class Database:
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+        self.sett = self.db.settings
+
+    def new_user(self, id, name):
+        return dict(
+            id=id,
+            name=name,
+            ban_status=dict(
+                is_banned=False,
+                ban_reason="",
+            ),
+        )
+
+    async def add_user(self, id, name):
+        user = self.new_user(id, name)
+        await self.col.insert_one(user)
+    
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return bool(user)
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_user(self, id):
+        return await self.col.find_one({'id': int(id)})
+
+    async def get_all_users(self):
+        return self.col.find({})
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def delete_all_users(self):
+        await self.col.delete_many({})
+        
+    async def remove_ban(self, id):
+        ban_status = dict(
+            is_banned=False,
+            ban_reason=''
+        )
+        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
+
+    async def ban_user(self, user_id, ban_reason="No Reason"):
+        ban_status = dict(
+            is_banned=True,
+            ban_reason=ban_reason
+        )
+        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
+
+    async def get_ban_status(self, id):
+        default = dict(
+            is_banned=False,
+            ban_reason=''
+        )
+        user = await self.col.find_one({'id': int(id)})
+        if not user:
+            return default
+        return user.get('ban_status', default)
+
+    async def get_banned(self):
+        users = self.col.find({'ban_status.is_banned': True})
+        b_users = [user['id'] async for user in users]
+        return b_users
+
+    async def get_db_size(self):
+        return (await self.db.command("dbstats"))['dataSize']
+
+    async def reset_database(self):
+        collections = await self.db.list_collection_names()
+        for collection in collections:
+            await self.db[collection].drop()
+        print("Database has been reset.")
+
+db = Database(DATABASE_URI, DATABASE_NAME)
