26ba3ad523230caacfd81f0e96f43d76aaddf1df
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-06-29 19:11:04 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -1,42 +1,94 @@
 import logging
-import logging.config
-import asyncio
-
-# Get logging configurations
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.INFO)
-
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, PORT
-from pyrogram import Client 
-from aiohttp import web
-from Plugins import web_server
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from Script import script
 from utils import temp
+from info import ADMINS 
 
-class Bot(Client):
-    def __init__(self):
-        super().__init__(
-            name=SESSION,
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            workers=20,
-            plugins={'root': 'Plugins'}
-        )
+logger = logging.getLogger(__name__)
 
-    async def start(self):
-        await super().start()
-        me = await self.get_me()
-        temp.U_NAME = me.username
-        print(f"New session started for {me.first_name}({me.username})")
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
+
+# Start command
+@Client.on_message(filters.command("start") & filters.private & filters.incoming)
+async def start(client, message):
+    start_buttons = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("Support Group ⚡", url="https://t.me/YourSupportGroup"),
+                InlineKeyboardButton("Update Channel 👨‍💻", url="https://t.me/YourUpdateChannel")
+            ],
+            [
+                InlineKeyboardButton("About", callback_data="about"),
+                InlineKeyboardButton("Help", callback_data="help"),
+                InlineKeyboardButton("Contact Us", url="https://t.me/YourContact")
+            ],
+            [
+                
+                InlineKeyboardButton("Usage", callback_data="usage"),
+                InlineKeyboardButton("Plans", callback_data="plans")
+            ]
+        ]
+    )
+    await message.reply_text(
+        text=script.START.format(user=message.from_user.mention),
+        disable_web_page_preview=True,
+        quote=True,
+        reply_markup=start_buttons
+    )
+
+@Client.on_message(filters.command('logs') & filters.user(ADMINS))
+async def log_file(bot, message):
+    try:
+        await message.reply_document('Logs.txt')
+    except Exception as e:
+        await message.reply(str(e))
         
-    async def stop(self):
-        await super().stop()
-        print("Session stopped. Bye!!")
+# About command
+@Client.on_message(filters.command("about") & filters.private & filters.incoming)
+async def about(client, message):
+    await message.reply_text(
+        text=script.ABOUT,
+        disable_web_page_preview=True,
+        quote=True,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Cancel", callback_data="cancel")]]
+        )
+    )
 
+# Help command
+@Client.on_message(filters.command("help") & filters.private & filters.incoming)
+async def help(client, message):
+    await message.reply_text(
+        text=script.HELP,
+        disable_web_page_preview=True,
+        quote=True,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Cancel", callback_data="cancel")]]
+        )
+    )
 
-app = Bot()
-app.run()
+# Feature command
+@Client.on_message(filters.command("feature") & filters.private & filters.incoming)
+async def feature(client, message):
+    bot=temp.U_NAME,
+    await message.reply_text(
+        text=script.FEATURE.format(botname=bot),
+        disable_web_page_preview=True,
+        quote=True,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Cancel", callback_data="cancel")]]
+        )
+    )
+
+# OTT command
+@Client.on_message(filters.command("ott") & filters.private & filters.incoming)
+async def ott(client, message):
+    await message.reply_text(
+        text=script.OTT.format(user=message.from_user.mention),
+        disable_web_page_preview=True,
+        quote=True,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Cancel", callback_data="cancel")]]
+        )
+    )
+
