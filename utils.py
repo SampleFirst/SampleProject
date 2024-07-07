adca342b081181f5e7d2ca773f09290b29f5b33c
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-07-07 13:02:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,3 +7,12 @@ logger.setLevel(logging.INFO)
 # temp db for banned 
 class temp(object):
     U_NAME = None
+
+def get_size(size):
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    size = float(size)
+    i = 0
+    while size >= 1024.0 and i < len(units):
+        i += 1
+        size /= 1024.0
+    return "%.2f %s" % (size, units[i])
